1a733e64feaa60e3439b059d1ed055e36ba8b212
jar4ik <you@example.com>
2024-11-04 14:32:32 +0000

    app.js changed

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ res.send('Hello World');
 });
 
 module.exports = app
-app.listen(PORT, () => {
-console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+console.log(`Server is running on http://localhost:${port}`);
 });
